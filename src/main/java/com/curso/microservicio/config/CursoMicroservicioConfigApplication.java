1ed43fbcd80c163b38630caca5f948fd587e1062
Hans Arias <hearias6@misena.edu.co>
2020-04-16 15:15:00 -0500

    configuracion inicial del config de microservicios

---
+++ src/main/java/com/curso/microservicio/config/CursoMicroservicioConfigApplication.java
@@ -0,0 +1,13 @@
+package com.curso.microservicio.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoMicroservicioConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoMicroservicioConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/microservicio/config/CursoMicroservicioConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.microservicio.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMicroservicioConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
